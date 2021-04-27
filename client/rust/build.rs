87aee474478f4841b5c844719ddf37a62d0d7493
Karl Bergström <karl.anton.bergstrom@gmail.com>
2021-04-28 00:11:35 +0200

    fix proto path

--- client/rust/build.rs
+++ client/rust/build.rs
@@ -1,4 +1,4 @@
 fn main() -> std::io::Result<()> {
-    prost_build::compile_protos(&["nh.proto", "nh_obj.proto"], &["../../nhvinst/", "proto"])?;
+    prost_build::compile_protos(&["nh.proto", "nh_obj.proto"], &["../../", "proto"])?;
     Ok(())
 }
