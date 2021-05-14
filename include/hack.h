1572d56c165700c9cf8233774899d4450135ae43
Hampus Linander <hampus.linander@gmail.com>
2021-05-14 14:19:19 +0200

    Hampes vinst

--- include/hack.h
+++ include/hack.h
@@ -576,7 +576,7 @@ enum optset_restrictions {
 #define DEVTEAM_EMAIL "devteam@nethack.org"
 #define DEVTEAM_URL "https://www.nethack.org/"
 
-#define PYTHON_BIN "/usr/bin/python"
+#define PYTHON_BIN (getenv("PYTHON_BIN") ? getenv("PYTHON_BIN") : "/usr/bin/python")
 
 typedef struct _team_bonus
 {
