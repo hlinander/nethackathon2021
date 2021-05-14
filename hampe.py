6502594fd0110a751cfd5ca0af7b47ccc9613561
Vinst <vinst@vinst.vi>
2021-05-14 15:44:00 +0200

    Vinst

--- hampe.py
+++ hampe.py
@@ -8,7 +8,7 @@ import sys
 import json
 import os
 
-NETHACK = "/home/herden/nh/install/games/lib/nethackdir/"
+NETHACK = "/home/nethack/nh/install/games/lib/nethackdir/"
 NETHACK_BIN = os.path.join(NETHACK, "nethack")
 
 banner = r'''             _,---.      ,----.         ___                    
@@ -54,15 +54,16 @@ def handle_play(states, stdscr):
     os.makedirs(ttydir, exist_ok=True)
     ttyfile = os.path.join(ttydir, str(random.randint(0, 1000)))
     command = f"ovh-ttyrec/ttyrec -f {ttyfile}.ttyrec -- sh -c 'DB_USER_ID={player_id} {NETHACK_BIN} -u {username}'"
-    #open("cmd", "w").write(command)
+    open("cmd", "w").write(command)
     os.system(command)
+    sys.exit()
 
 
 states = dict(
     login=dict(prompt=True, prompt_text="username: ", y=20, buffer="", next_state = "password"),
     password=dict(prompt=True, prompt_text="password: ", y=20, buffer="", next_state = "validate"),
-    validate=dict(prompt=False, func=handle_login),
-    play=dict(prompt=False, func=handle_play)
+    validate=dict(prompt=False, func=handle_login, buffer="", next_state="play"),
+    play=dict(prompt=False, func=handle_play, buffer="", next_state="play")
 )
 
 
@@ -91,28 +92,33 @@ def main(stdscr):
             # stdscr.addstr(23, 32, f'Team Power gems: {power_gems:03d}')
             # stdscr.addstr(24, 8, error, error_color)
         if "func" in states[current_state]:
+            open("state","a").write(current_state)
             current_state = states[current_state]["func"](states, stdscr)
+            open("state","a").write(current_state)
+        open("state","a").write(current_state)
         if states[current_state]["prompt"]:
             stdscr.addstr(states[current_state]["y"], 8, states[current_state]["prompt_text"])
             stdscr.addstr(states[current_state]["y"] + 1, 8, " "*20)
             stdscr.addstr(states[current_state]["y"] + 1, 8, states[current_state]["buffer"])
 
         stdscr.refresh()
-        try:
-            v = stdscr.getch()
-            if -1 == v:
-                continue
-            if v == 263: # Backspace
-                states[current_state]["buffer"] = states[current_state]["buffer"][:-1]
-            elif v == 10:
-                current_state = states[current_state]["next_state"]
-                stdscr.clear()
-                # Enter
-            else:
-                states[current_state]["buffer"] += chr(v)
-            raise Exception(v)
+        #try:
+        v = stdscr.getch()
+        if -1 == v:
+            continue
+        if v == 263: # Backspace
+            states[current_state]["buffer"] = states[current_state]["buffer"][:-1]
+        elif v == 10:
+            open("state", "w").write(current_state)
+            current_state = states[current_state]["next_state"]
+            open("state", "a").write(current_state)
+            stdscr.clear()
+            # Enter
+        else:
+            states[current_state]["buffer"] += chr(v)
+        #raise Exception(v)
 
-        except Exception as e:
-            open('/tmp/fisk', 'w').write(str(e) + '\n' + traceback.format_exc())
+        #except Exception as e:
+        #    open('/tmp/fisk', 'w').write(str(e) + '\n' + traceback.format_exc())
 
 wrapper(main)
