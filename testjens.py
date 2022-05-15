a604cbde4452506c036010cb0744d0b117062ab7
Hampus Linander <hampus.linander@gmail.com>
2022-05-15 14:39:30 +0200

    Loop testjens

--- testjens.py
+++ testjens.py
@@ -3,10 +3,15 @@
 import jens
 import db
 import datetime
+import time
+import sys
 
 db.open_db()
-hunger = db.get_stonk(1, "hunger", 100, datetime.datetime.now() - datetime.timedelta(hours=20), datetime.datetime.now())
-hp = db.get_stonk(1, "hp", 100, datetime.datetime.now() - datetime.timedelta(hours=20), datetime.datetime.now())
-jens.chart_in_box(10, 10, 100, 20, hunger, "hunger", 1)
-jens.chart_in_box(10, 35, 100, 20, hp, "hp", 1)
-input()
+while True:
+    hunger = db.get_stonk(1, "hunger", 100, datetime.datetime.utcnow() - datetime.timedelta(minutes=1), datetime.datetime.utcnow())
+    hp = db.get_stonk(1, "hp", 100, datetime.datetime.utcnow() - datetime.timedelta(minutes=1), datetime.datetime.utcnow())
+    jens.clear()
+    jens.chart_in_box(10, 1, 100, 20, hunger, "hunger", 1)
+    jens.chart_in_box(10, 25, 100, 20, hp, "hp", 1)
+    sys.stdout.flush()
+    time.sleep(1)
