c10162affb382a469beb38c7a56f616e87afe3f4
Karl Bergström <karl.anton.bergstrom@gmail.com>
2022-05-26 16:13:29 +0200

    fiskapa

--- fiskapa.py
+++ fiskapa.py
@@ -5,6 +5,8 @@ import re
 import sys
 import random
 import os
+import db
+import session
 import jens
 from subprocess import Popen
 
@@ -36,11 +38,11 @@ def _load_tty(filename):
 	stream = pyte.Stream(screen)
 	fp = open(filename, 'rb')
 	return dict(fp=fp, screen=screen, stream=stream, start_at=None, next_at=None,
-		name='Namnet Sko', hp=0, maxhp=0, money=0, dead=False)
+		hp=0, maxhp=0, money=0, dead=False)
 
 def load_tty(user_id, filename):
 	return { 'user_id': user_id, 'live': _load_tty(filename), 'death': _load_tty(filename),
-		'created': time.time() }
+		'filename': filename, 'created': time.time() }
 
 def read_time(tr):
 	try:
@@ -201,53 +203,114 @@ def death_cam(tr, start):
 def update_rec_list(ttydir, recs):
 	files_by_user_id = {}
 	for (dirpath, dirnames, filenames) in os.walk(ttydir):
-		latest = None
 		for file in filenames:
+			file = os.path.join(dirpath, file)
 			if file.endswith(".ttyrec"):
-				mtime = os.path.getmtime(file)
-				if latest is None or latest["mtime"] < mtime:
-					m = re.match(r'.*\d+_(\d+)\.ttyrec', file)
-					if m is not None:
-						latest = {
-							"user_id": int(m.group(1)),
+				m = re.match(r'.*\d+_(\d+)\.ttyrec', file)
+				if m is not None:
+					user_id = int(m.group(1))
+					latest = files_by_user_id.get(user_id)
+					mtime = os.path.getmtime(file)
+					if latest is None or latest["mtime"] < mtime:
+						files_by_user_id[user_id] = {
+							"user_id": user_id,
 							"mtime": mtime,
 							"path": file
 						}
-		if latest is not None:
-			files_by_user_id[latest["user_id"]] = latest
 	
 	for k, v in files_by_user_id.items():
 		found = False
 		for i in range(0, len(recs)):
 			it = recs[i]
-			if k == it['user_id'] and it['filename'] != v['path']:
-				recs[i] = load_tty(k, v['path'])
+			if k == it['user_id']:
+				if it['filename'] != v['path']: # new file
+					recs[i] = load_tty(k, v['path'])
 				found = True
 				break
 		if not found:
 			recs.append(load_tty(k, v['path']))
 
-def is_critical(rec):
-	return (it['live']['hp'] / it['live']['maxhp']) < 0.5
+
+def get_long_amount(player_state):
+	long_amount_gems = 0
+	if 'long_amount_gems' in player_state:
+		long_amount_gems = player_state['long_amount_gems']
+	return long_amount_gems
+
+def get_short_amount(player_state):
+	short_amount_gems = 0
+	if 'short_amount_gems' in player_state:
+		short_amount_gems = player_state['short_amount_gems']
+	return short_amount_gems
+
+
+
+def interest_level(rec, all_player_states):
+	if 'player_state' in rec:
+		player_state = rec['player_state']
+		if 'hp' in player_state and 'max_hp' in player_state and player_state['max_hp'] != 0:
+			hp_percentage = player_state['hp'] / player_state['max_hp']
+		else:
+			hp_percentage = 1.0
+
+		total_investment = 0
+		for player in all_player_states:
+			total_investment += get_long_amount(player) + get_short_amount(player)
+
+		player_investment = get_long_amount(player) + get_short_amount(player)
+
+		investment_interest = player_investment / total_investment if total_investment != 0 else 0
+		
+		return 1.0 - hp_percentage + investment_interest
+	return 0
+
+def rec_interest_sort_key(rec, all_player_states):
+	return interest_level(rec, all_player_states)
+
 
 def choose_on_display(on_display, recs):
-	if len(on_display) < 4:
-		for it in recs:
-			if not it['live']['dead']:
-				on_display.append({ 'rec': it, 'live_at': time.time() })
-				if 4 == len(on_display):
-					break
+	player_states = session.get_state()['players']
+
+	# update player_state in recs
 	for it in recs:
-		if is_critical(it):
-			if it not in on_display:
-				for i in range(0, len(on_display)):
-					if not is_critical(on_display[i]['rec']):
-						on_display[i]['rec'] = it
-						on_display[i]['live_at'] = time.time()
-						break
-	for it in on_display:
-		if it['live_at'] + 15 < time.time():
-			pass
+		if str(it['user_id']) in player_states:
+			player_state = player_states[str(it['user_id'])]
+			it['player_state'] = player_state
+			it['interest'] = interest_level(it, player_states)
+
+	sorted_recs = sorted(recs, reverse=True, key=lambda x: rec_interest_sort_key(x, player_states))
+	
+	sorted_not_displayed = list(filter(lambda x: not any(o['rec'] == x for o in on_display), sorted_recs))
+
+	for i in range(4):
+		if len(on_display) <= i:
+			if len(sorted_not_displayed) > 0:
+				on_display.append( {
+					'rec': sorted_not_displayed[0],
+					'live_at': time.time(),
+				})
+				del sorted_not_displayed[0]
+		else:
+			current = on_display[i]
+			time_live = time.time() - current['live_at']
+			if time_live > 15:
+				current_interest_level = interest_level(current['rec'], player_states)
+				current_interest_level *= 1.0 - min(time_live / 60.0, 1.0) * 0.85
+				if len(sorted_not_displayed) > 0:
+					other = sorted_not_displayed[0]
+					other_interest_level = interest_level(other, player_states)
+					if other_interest_level > current_interest_level:
+						# check if something is more interesting
+						on_display[i] = {
+							'rec': other,
+							'live_at': time.time(),
+						}
+						del sorted_not_displayed[0]
+
+	for i in range(4):
+		if len(on_display) > i:
+			on_display[i]['x'] = 1 + (i & 1) * MON_W + (i&1)
+			on_display[i]['y'] = 1 + (i >> 1) * MON_H + (i>>1)
 
 
 def run(ttydir):
@@ -260,11 +323,18 @@ def run(ttydir):
 		choose_on_display(on_display, recs)
 		framedata = []
 		time.sleep(1/30)
-		for i in range(0, len(on_display)):
-			screen_x = 1 + (i & 1) * MON_W + (i&1)
-			screen_y = 1 + (i >> 1) * MON_H + (i>>1)
-			it = recs[i]
-			run_tty(it['live'], start, screen_x, screen_y, it in on_display)
+		i = 0
+		for it in recs:
+			screen_x = 0
+			screen_y = 0
+			exists = False
+			for d in on_display:
+				if d['rec'] == it:
+					exists = True
+					screen_x = d['x']
+					screen_y = d['y']
+					break
+			run_tty(it['live'], start, screen_x, screen_y, exists)
 			if it['live']['dead']:
 				death_cam(it['death'], start + DEATH_CAM_SEC)
 			elif time.time() > (it['created'] + DEATH_CAM_SEC):
@@ -273,7 +343,13 @@ def run(ttydir):
 				color = int(4 - (3 * (it['live']['hp'] / it['live']['maxhp'])))
 			else:
 				color = 0
-			framedata.append((screen_x, screen_y, MON_W+2, MON_H+2, it['live']['name'], color))
+			if 'player_state' in it:
+				player_state = it['player_state']
+			if player_state and 'player_name' in player_state:
+				player_name = player_state['player_name'] + " (" + str(it['interest']) + ")"
+			else:
+				player_name = "Loading.."
+			framedata.append((screen_x, screen_y, MON_W+2, MON_H+2, player_name, color))
 		reset()
 		jens.paint_frames(framedata)
 		recs = [it for it in recs if not it['live']['dead']]
@@ -297,6 +373,7 @@ def set_fullscreen_size():
 	MON_W = MON_W - 2
 	MON_H = MON_H - 2 # borders
 
+db.open_db()
 show_cursor(False)
 set_4x4_size()
 load_font('nhmon/font.txt')
