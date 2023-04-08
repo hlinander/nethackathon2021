eaec896de4fff7ba72fe98c25f063ca968135962
Karl Bergström <karl.anton.bergstrom@gmail.com>
2023-04-08 19:53:19 +0200

    fix input select wait loop

--- client/rust/src/oracle.rs
+++ client/rust/src/oracle.rs
@@ -1,13 +1,16 @@
 use std::{
+    ffi::{CStr, CString},
     io::{stdin, stdout, Read, Write},
-    os::unix::prelude::AsFd,
+    os::unix::prelude::{AsFd, AsRawFd},
     time::Duration,
 };
 
 use crossterm::{cursor::*, terminal::Clear, *};
 use libc::abort;
+use nethack_rs::g;
 use reqwest::Client;
 use serde::Serialize;
+use termios::{tcgetattr, tcsetattr, Termios, ICANON};
 use tokio::{
     runtime::Runtime,
     sync::mpsc::{
@@ -18,6 +21,20 @@ use tokio::{
 };
 
 static mut TOKIO_RUNTIME: Option<tokio::runtime::Runtime> = None;
+
+enum MapObjectID {
+    Item,
+    Monster,
+    Tile,
+}
+
+struct MapObject {
+    id: MapObjectID,
+    name: String,
+    symbol: String,
+    color: String,
+    distance: f32,
+}
 
 enum ReceiveMsg {
     MoreData(String),
@@ -39,6 +56,19 @@ static mut REQUEST_ID: u32 = 0;
 pub unsafe fn init() {
     assert!(TOKIO_RUNTIME.is_none());
     TOKIO_RUNTIME = Some(Runtime::new().unwrap());
+    let stdin_fd = std::io::stdin().as_raw_fd();
+
+    // get the current terminal attributes
+    let mut termios = Termios::from_fd(stdin_fd).unwrap();
+
+    // disable input buffering
+    termios.c_lflag &= !(libc::ECHO | libc::ICANON);
+    termios.c_cc[termios::VMIN] = 1;
+    termios.c_cc[termios::VTIME] = 0;
+
+    // set the new terminal attributes
+    tcsetattr(stdin_fd, libc::TCSANOW, &termios).unwrap();
+    tcsetattr(stdin_fd, libc::TCSAFLUSH, &termios).unwrap();
 }
 
 #[derive(Serialize)]
@@ -106,6 +136,8 @@ pub unsafe extern "C" fn oracle_prompt() -> i32 {
     execute!(stdout(), MoveTo(0, 25), EnableBlinking).unwrap();
     loop {
         let c = nethackathon_getch();
+        print!("{}", c);
+        stdout().flush();
         if c == 8 || c == 127 {
             // backspace
             if !line.is_empty() {
@@ -178,113 +210,102 @@ unsafe fn update_status_line() {
     }
 }
 
+pub unsafe fn update_oracle_ui() {
+    if UI_STATE.is_none() {
+        UI_STATE = Some(UiState {
+            cursor_pos: default_cursor_pos(),
+            ..Default::default()
+        });
+    }
+    let ui_state = UI_STATE.as_mut().unwrap();
+    let request_id = update_request_state(ui_state);
+    let mut clear = false;
+    if let Some(current_id) = request_id {
+        if ui_state.last_request_id != current_id {
+            clear = true;
+            ui_state.last_request_id = current_id;
+            ui_state.request_status = RequestStatus::Active;
+        }
+    }
+    let mut needs_update = ui_state.received_text != ui_state.printed_text || clear;
+    update_status_line();
+    if needs_update {
+        if clear {
+            execute!(
+                stdout(),
+                SavePosition,
+                MoveTo(default_cursor_pos().0, default_cursor_pos().1),
+                Clear(terminal::ClearType::FromCursorDown),
+                RestorePosition
+            )
+            .unwrap();
+            ui_state.cursor_pos = default_cursor_pos();
+        }
+        let (x, y) = ui_state.cursor_pos;
+        execute!(stdout(), SavePosition, MoveTo(x, y)).unwrap();
+        if ui_state.printed_text.len() < ui_state.received_text.len() {
+            let diff = &ui_state.received_text[ui_state.printed_text.len()..];
+            ui_state.printed_text += diff;
+            print!("{}", diff);
+        }
+        ui_state.cursor_pos = crossterm::cursor::position().unwrap();
+        execute!(stdout(), RestorePosition).unwrap();
+        stdout().flush().unwrap();
+    }
+}
+
+unsafe fn update_request_state(ui_state: &mut UiState) -> Option<u32> {
+    let mut request_id = None;
+    if let Some(req) = ACTIVE_REQUEST.as_mut() {
+        request_id = Some(req.id);
+        match req.data_receive.try_recv() {
+            Ok(ReceiveMsg::MoreData(data)) => {
+                ui_state.received_text += &data;
+            }
+            Ok(ReceiveMsg::Error(error_msg)) => {
+                ui_state.request_status = RequestStatus::Error(error_msg);
+                abort_active_request();
+            }
+            Ok(ReceiveMsg::End) | Err(TryRecvError::Disconnected) => {
+                ui_state.request_status = RequestStatus::Done;
+                abort_active_request();
+            }
+            Err(TryRecvError::Empty) => {}
+        }
+    }
+    request_id
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn nethackathon_getch() -> i32 {
-    let mut update_ui = || {
-        if UI_STATE.is_none() {
-            UI_STATE = Some(UiState {
-                cursor_pos: default_cursor_pos(),
-                ..Default::default()
-            });
-        }
-        let ui_state = UI_STATE.as_mut().unwrap();
-        let mut request_id = None;
-        if let Some(req) = ACTIVE_REQUEST.as_mut() {
-            request_id = Some(req.id);
-            match req.data_receive.try_recv() {
-                Ok(ReceiveMsg::MoreData(data)) => {
-                    ui_state.received_text += &data;
-                }
-                Ok(ReceiveMsg::Error(error_msg)) => {
-                    ui_state.request_status = RequestStatus::Error(error_msg);
-                    abort_active_request();
-                }
-                Ok(ReceiveMsg::End) | Err(TryRecvError::Disconnected) => {
-                    ui_state.request_status = RequestStatus::Done;
-                    abort_active_request();
-                }
-                Err(TryRecvError::Empty) => {}
-            }
-        }
-        let mut clear = false;
-        if let Some(current_id) = request_id {
-            if ui_state.last_request_id != current_id {
-                clear = true;
-                ui_state.last_request_id = current_id;
-                ui_state.request_status = RequestStatus::Active;
-            }
-        }
-        let mut needs_update = ui_state.received_text != ui_state.printed_text || clear;
-        libc::fcntl(
-            0,
-            libc::F_SETFL,
-            libc::fcntl(0, libc::F_GETFL) & !libc::O_NONBLOCK,
-        );
-        update_status_line();
-        if needs_update {
-            if clear {
-                execute!(
-                    stdout(),
-                    SavePosition,
-                    MoveTo(default_cursor_pos().0, default_cursor_pos().1),
-                    Clear(terminal::ClearType::FromCursorDown),
-                    RestorePosition
-                )
-                .unwrap();
-                ui_state.cursor_pos = default_cursor_pos();
-            }
-            let (x, y) = ui_state.cursor_pos;
-            execute!(stdout(), SavePosition, MoveTo(x, y)).unwrap();
-            if ui_state.printed_text.len() < ui_state.received_text.len() {
-                let diff = &ui_state.received_text[ui_state.printed_text.len()..];
-                ui_state.printed_text += diff;
-                print!("{}", diff);
-            }
-            ui_state.cursor_pos = crossterm::cursor::position().unwrap();
-            execute!(stdout(), RestorePosition).unwrap();
-            stdout().flush().unwrap();
-        }
-    };
-
     let mut selector = selecting::Selector::new();
-    selector.add_read(&stdin().lock());
-    update_ui();
+    selector.add_read(&0);
+    update_oracle_ui();
     loop {
-        libc::fcntl(
-            0,
-            libc::F_SETFL,
-            libc::fcntl(0, libc::F_GETFL) | libc::O_NONBLOCK,
+        let mut rfds: libc::fd_set = std::mem::zeroed();
+        libc::FD_SET(0, &mut rfds);
+
+        let mut tv = libc::timeval {
+            tv_sec: 0,
+            tv_usec: 200000,
+        };
+
+        let retval = libc::select(
+            1,
+            &mut rfds as _,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            &mut tv as _,
         );
-        // let mut byte = [0u8; 4];
-        match libc::getchar() {
-            -1 => update_ui(),
-            c => {
-                libc::fcntl(
-                    0,
-                    libc::F_SETFL,
-                    libc::fcntl(0, libc::F_GETFL) & !libc::O_NONBLOCK,
-                );
-                return c;
-            }
+        if retval == -1 {
+            eprintln!("select() failed: {}", std::io::Error::last_os_error());
+        } else if retval > 0 {
+            println!("reading...");
+            let c = libc::getchar();
+            println!("read");
+            return c;
+        } else {
+            update_oracle_ui();
         }
-        std::thread::sleep(Duration::from_millis(1));
-        // match selector.select_timeout(Duration::from_millis(200)) {
-        //     Ok(result) => {
-        //         if result.is_read(&stdin()) {
-        //             // let mut buf = [0u8; 4];
-        //             // if libc::feof(stdin().lock().) > 0 {
-        //             //     // let result = stdin().lock().read(&mut buf).unwrap();
-        //             //     // return i32::from_le_bytes(buf);
-        //             //     return libc::getchar();
-        //             // }
-        //         } else {
-        //             update_ui();
-        //         }
-        //     }
-        //     Err(err) if err.kind() == std::io::ErrorKind::TimedOut => {
-        //         update_ui();
-        //     }
-        //     Err(err) => panic!("select error {:?}", err),
-        // }
     }
 }
