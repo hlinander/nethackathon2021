627d9ff323d520dff2e08ec9767218ad29dc30c1
Karl Bergström <karl.anton.bergstrom@gmail.com>
2023-04-09 02:44:48 +0200

    regen.py

---
+++ client/rust/nethack-rs/regen.py
@@ -0,0 +1,40 @@
+import subprocess
+
+data = subprocess.check_output(["bindgen", "../../../client/NetHack/include/hack.h"]) 
+
+s = data.decode()
+
+search = """pub fn new_bitfield_1(
+        flags: ::std::os::raw::c_uint,
+        horizontal: ::std::os::raw::c_uint,
+        lit: ::std::os::raw::c_uint,
+        waslit: ::std::os::raw::c_uint,
+        roomno: ::std::os::raw::c_uint,
+        edge: ::std::os::raw::c_uint,
+        candig: ::std::os::raw::c_uint,
+    ) -> __BindgenBitfieldUnit<[u8; 2usize]> {
+        let mut __bindgen_bitfield_unit: __BindgenBitfieldUnit<[u8; 2usize]> = Default::default();
+        __bindgen_bitfield_unit.set(0usize, 5u8, {
+            let flags: u32 = unsafe { ::std::mem::transmute(flags) };
+            flags as u64
+        });"""
+
+replacement = """pub fn new_bitfield_1(
+        flags_: ::std::os::raw::c_uint,
+        horizontal: ::std::os::raw::c_uint,
+        lit: ::std::os::raw::c_uint,
+        waslit: ::std::os::raw::c_uint,
+        roomno: ::std::os::raw::c_uint,
+        edge: ::std::os::raw::c_uint,
+        candig: ::std::os::raw::c_uint,
+    ) -> __BindgenBitfieldUnit<[u8; 2usize]> {
+        let mut __bindgen_bitfield_unit: __BindgenBitfieldUnit<[u8; 2usize]> = Default::default();
+        __bindgen_bitfield_unit.set(0usize, 5u8, {
+            let flags_: u32 = unsafe { ::std::mem::transmute(flags_) };
+            flags_ as u64
+        });"""
+
+s = s.replace(search, replacement)
+
+
+open("src/lib.rs", 'w').write(s)
